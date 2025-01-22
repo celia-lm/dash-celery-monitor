b6bf0ac81f04c517543a0df2961aaf8fe8deffe8
workspaceuser <workspaceuser@plot.ly>
2025-01-22 09:43:16 +0000

    One Click Deploy - 1737538995472

---
+++ app.py
@@ -0,0 +1,313 @@
+import time
+import os
+import datetime
+import dash
+from dash import Dash, DiskcacheManager, CeleryManager
+from dash import Input, Output, State, ctx, html, dcc, callback, set_props
+from celery import Celery, worker
+import dash_ag_grid as dag
+from icecream import ic
+import utils
+import dash_bootstrap_components as dbc
+
+celery_app = Celery(
+    __name__,
+    broker=f"{os.environ['REDIS_URL']}/3",
+    backend=f"{os.environ['REDIS_URL']}/4",
+)
+celery_inspector = celery_app.control.inspect()
+CELERY_HOSTNAME = worker.worker.WorkController(app=celery_app).hostname
+
+app = Dash(update_title=None, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
+
+def layout():
+
+    # populate the table with tasks that have been sent prior to the page load
+    initial_celery_data = utils.get_celery_active_and_reserved(celery_inspector, CELERY_HOSTNAME)
+    
+    return dbc.Container(
+        [
+            html.H2("Celery Monitor App"),
+            utils.app_description,
+            # components to trigger celery tasks
+            html.H4("Tasks to test celery", style={"padding-top":"2px"}),
+            utils.task_description,
+            dbc.Row([
+                dbc.Col(dbc.Card(dbc.CardBody([
+                    dbc.Button(id="button_1", children="Run task 1! (2 min)", style={"margin":"2px"}),
+                    html.P(id="paragraph_1", children=["Button for task 1 not clicked"])
+                ]), style={"height":"175px"})),
+                dbc.Col(dbc.Card(dbc.CardBody([
+                    html.Span("Select duration (in minutes) for task 2:"),
+                    dcc.Input(id="task_2_len", type="number", min=1, step=1, value=1), html.Br(),
+                    dbc.Button(id="button_2", children="Run task 2!", style={"margin":"2px"}),
+                    html.P(id="paragraph_2", children=["Button for task 2 not clicked"]),
+                ]), style={"height":"175px"}
+                ))
+            ], style={"margin-botton":"5px"}), 
+            # table to display the task checks with interval to update it every 5 seconds
+            dcc.Interval(
+                id="interval", interval=1000 * 5, disabled=True
+            ),  
+            dbc.Button(
+                id="check_celery", children="Check celery status and update table"
+            ),
+            html.Div(id="check_celery_output"),
+            dag.AgGrid(
+                id="dag_celery",
+                rowData=initial_celery_data,
+                columnDefs=[
+                    {"field": c, "label": c}
+                    # fields from TASK_INFO: https://docs.celeryq.dev/en/latest/reference/celery.app.control.html#celery.app.control.Inspect.query_task
+                    # you could add your own custom fields and updates, for example: "triggered_by", or "cancelled_at"
+                    # "time_start" indicates the time the task was SENT to celery, not the time it actually started running
+                    for c in [
+                        "id",
+                        "name",
+                        "args",
+                        "kwargs",
+                        "time_start",
+                        "time_end",
+                        "status",
+                    ]
+                ],
+                getRowId="params.data.id",
+                dashGridOptions={"rowSelection": "single"},
+            ),
+            dbc.Button(id="cancel_task", children="Cancel selected task", disabled=True),
+        ], style={"padding":"10px"}
+    )
+
+app.layout = layout
+
+# structure: task_name : {"kwargs":[arg1, arg2], "output"}
+# this is not necessary for the monitor itself; it's for updating the layout when the tasks finish running
+# the monitor will work too for tasks with no output
+TASK_OUTPUTS = {
+    "my_task_1": {"component_id": "paragraph_1", "component_prop": "children"},
+    "my_task_2": {"component_id": "paragraph_2", "component_prop": "children"},
+}
+
+# I've created two simple tasks
+@celery_app.task(name="my_task_1")
+def mytask1_wrapped(n_clicks):
+    time.sleep(60 * 2)
+    # print to check if the task keeps running after being cancelled
+    # ic is not celery-friendly
+    print(f"Finishing task 1 with {n_clicks}")
+    return f"task 1: Clicked {n_clicks} times completed at {datetime.datetime.now()}"
+
+
+@celery_app.task(name="my_task_2")
+def mytask2_wrapped(n_clicks, len_min):
+    # prints to check if the task keeps running after being cancelled
+    for i in range(len_min):
+        if i == 0 : 
+            print(f"my_task_2 started with expected duration of {len_min} min. Current time is: {datetime.datetime.now()}")
+        else : 
+            print(f"{i} min have passed at {datetime.datetime.now()}")
+        time.sleep(60)
+
+    print(f"Finishing task 2 with n_clicks={n_clicks} and len={len_min}")
+    return f"task 2: Clicked {n_clicks} times completed at {datetime.datetime.now()}"
+
+
+# callback with no output for improved performance
+# https://dash.plotly.com/advanced-callbacks#callbacks-with-no-outputs
+@callback(
+    Input("button_1", "n_clicks"),
+    Input("button_2", "n_clicks"),
+    State("task_2_len", "value"),
+    prevent_initial_call=True,
+)
+def update_clicks(n_clicks_1, n_clicks_2, len_min):
+    if ctx.triggered:
+        k, v = list(ctx.triggered_prop_ids.items())[0]  # there will only be one item
+
+        # I haven't found a smart way to automate this since every task (in a more complex app)
+        # could have very different kwargs and the objects passed as callback args different names
+        # (e.g. n_clicks_1 for the n_clicks argument)
+        task_kwargs = {}
+        if v == "button_1":
+            task_name = "my_task_1"
+            task_kwargs = {"n_clicks": n_clicks_1}
+        elif v == "button_2":
+            task_name = "my_task_2"
+            task_kwargs = {"n_clicks": n_clicks_2, "len_min":len_min}
+        # for error handling:
+        else:
+            task_name = None
+
+        if task_name:
+            task_id = celery_app.send_task(task_name, kwargs=task_kwargs)
+            task_id_str = str(task_id)
+            triggered_at = datetime.datetime.now()
+            # https://dash.plotly.com/dash-ag-grid/client-side#transaction-updates
+            newRows = [
+                {
+                    "id": task_id_str,
+                    "name": task_name,
+                    "kwargs": str(task_kwargs),
+                    "time_start": triggered_at.strftime("%H:%M:%S"),
+                    "time_end": None,
+                    "status": "Queued",
+                }
+            ]
+            dash.set_props("dag_celery", {"rowTransaction": {"add": newRows}})
+
+    # no return statement
+
+
+# this updates the "disabled" property of the interval, making it start running or stop
+# other updates to the grid with the task info are done via set_props
+@callback(
+    Output("interval", "disabled"),
+    Input("dag_celery", "rowData"),
+    Input("interval", "n_intervals"),
+    Input("check_celery", "n_clicks"),
+    State("interval", "disabled"),
+    prevent_initial_call=True,
+)
+def check_task_status(current_tasks, _intervals, _check_celery, _disabled):
+    # if there are tasks and none of them is pending, stop interval
+    # all(...) will return true if current_tasks is empty too, that's why we add "if current_tasks and"
+    if current_tasks and all(
+        [task["status"] in ["Cancelled", "Complete"] for task in current_tasks]
+    ):
+        return True  # stop interval
+    elif ctx.triggered_id == "dag_celery":
+        # start interval if it isn't running yet
+        return False if _disabled else dash.no_update
+    # if it's the interval what triggers the callback, run the check for tasks' status
+    elif ctx.triggered_id in ["interval", "check_celery"]:
+        for task_dict in current_tasks:
+            # don't do anything with tasks that have already been cancelled or completed
+            if task_dict["status"] in ["Cancelled", "Complete"]:
+                res = celery_app.AsyncResult(task_id)
+                ic(task_id, celery_inspector.query_task(task_id), res.status)
+                continue
+            # if task is Queued or Running
+            else:
+                task_id = task_dict["id"]
+                res = celery_app.AsyncResult(task_id)
+                ic(task_id, celery_inspector.query_task(task_id), res.status)
+                # double check in case of concurrent callbacks
+                if res.status == "REVOKED":
+                    continue
+                # task finished
+                elif res.ready():
+                    # more info about disable_sync_subtasks: https://docs.celeryq.dev/en/latest/userguide/tasks.html#avoid-launching-synchronous-subtasks
+                    result = res.get(disable_sync_subtasks=False)
+                    output_info = TASK_OUTPUTS.get(task_dict["name"])
+                    dash.set_props(
+                        output_info.get("component_id"),
+                        {output_info.get("component_prop"): result},
+                    )
+
+                    dash.set_props(
+                        "dag_celery",
+                        {
+                            "rowTransaction": {
+                                "update": [
+                                    utils.update_row_value(
+                                        task_dict,
+                                        {
+                                            "status": "Complete",
+                                            "time_end": datetime.datetime.now().strftime(
+                                                "%H:%M:%S"
+                                            ),
+                                        },
+                                    )
+                                ]
+                            }
+                        },
+                    )
+                else:
+                    # task_state is one of: "active", "reserved"
+                    # it's different from res.status, which can be ACTIVE, REVOKED, PENDING
+                    task_state = celery_inspector.query_task(task_id)[CELERY_HOSTNAME][task_id][0]
+                    if task_state == "reserved":
+                        continue
+                    # only update the grid if it hasn't been updated yet
+                    elif task_dict["status"] != "Running":
+                        dash.set_props(
+                            "dag_celery",
+                            {
+                                "rowTransaction": {
+                                    "update": [
+                                        utils.update_row_value(
+                                            task_dict, {"status": "Running"}
+                                        )
+                                    ]
+                                }
+                            },
+                        )
+                    else:
+                        continue
+
+        return dash.no_update
+
+@callback(
+    Input("cancel_task", "n_clicks"),
+    State("dag_celery", "selectedRows"),
+    prevent_initial_row=True,
+)
+def cancel_job(click, selectedRows):
+    task_dict = selectedRows[0]
+    res1 = celery_app.AsyncResult(task_dict["id"])
+    celery_app.control.revoke(task_dict["id"], terminate=True)
+    dash.set_props(
+        "dag_celery",
+        {
+            "rowTransaction": {
+                "update": [utils.update_row_value(task_dict, {"status": "Cancelled"})]
+            }
+        }
+    )
+
+# callbacks for performing checks (it takes 3-10 seconds, depending on the amount of tasks)
+@callback(
+    Output("check_celery_output", "children"),
+    Input("check_celery", "n_clicks"),
+    State("dag_celery", "rowData"),
+    prevent_initial_call=True,
+)
+def celery_status(_, current_tasks):
+    task_queries = [
+        celery_inspector.query_task(task_dict["id"]) for task_dict in current_tasks
+    ]
+    text = f"""
+    **Active tasks:** 
+    ```
+    {celery_inspector.active()}
+    ```
+    **Revoked tasks:** 
+    ```
+    {celery_inspector.revoked()}
+    ```
+    **Reserved tasks:** 
+    ```
+    {celery_inspector.reserved()}
+    ```
+    ## Information by task_id
+    ```
+    {task_queries}
+    ```
+    """
+    return utils.celery_status_summary(text)
+
+# auxiliar callback to disable the cancel button if no row is selected
+@callback(
+    Output("cancel_task", "disabled"),
+    Input("dag_celery", "selectedRows"),
+    prevent_initial_call=True
+)
+def disable_button(selectedRows):
+    if selectedRows:
+        return False
+    else :
+        return True
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,145 @@
+from dash import dcc, html
+import dash_bootstrap_components as dbc
+import datetime
+
+app_description_text = """
+The purpose of this app is to allow the user to interact with celery tasks
+and see how we can get insights abour the different phases
+of the task completition process programmatically (i.e. by using the celery python library).\n
+The complete code can be reviewed in the [GitHub repository](link), but here's a summary
+of the main actions and takeaways from the design process of this app.\n
+
+Main reference: https://docs.celeryq.dev/en/latest/reference/celery.app.control.html
+
+**Set-up**
+```
+celery_app = Celery(
+    __name__,
+    broker=f"{os.environ['REDIS_URL']}/1",
+    backend=f"{os.environ['REDIS_URL']}/2",
+)
+
+# code to send a task inside a callback
+task_id = celery_app.send_task(task_name, kwargs=task_kwargs)
+```
+To save this task id (for example, in a dcc.Store) we will need to convert it to str with `str(task_id)`. 
+In the rest of this explanation, when `task_id` is used, it refers to the str version.
+
+**Check the status of a task**\n
+Option A (more general):
+```
+celery_inspector.query_task(task_id)
+```
+Example of result:
+```
+{'celery@27a8ec03a114': {'94be3755-011c-424e-a583-5fc73fd627ef': ['active', {'id': '94be3755-011c-424e-a583-5fc73fd627ef', 'name': 'my_task_1', 'args': [], 'kwargs': {'n_clicks': 1}, 'type': 'my_task_1', 'hostname': 'celery@27a8ec03a114', 'time_start': 1737474277.8087084, 'acknowledged': True, 'delivery_info': {'exchange': '', 'routing_key': 'celery', 'priority': 0, 'redelivered': False}, 'worker_pid': 4385}]}}
+```
+For this app, we are mainly interested in the task state, which can be: `active` or `reserved`. \n
+When a task has been revoked/cancelled, the output will be a dict with an emmpty dict for value, like this:
+```
+{'celery@27a8ec03a114': {}}
+```
+
+Option B ([ref](https://docs.celeryq.dev/en/latest/reference/celery.result.html#celery.result.AsyncResult))
+```
+res = celery_app.AsyncResult(task_id)
+print(res.status)
+res.ready() # true when task has finished
+res.get() # to retrieve the output of the task
+```
+
+**Commands to inspect the current tasks** ([ref](https://docs.celeryq.dev/en/v5.4.0/reference/celery.app.control.html#celery.app.control.Inspect.query_task))
+```
+celery_inspector = celery_app.control.inspect()
+# tasks that are currently being executed by a worker
+active_tasks = celery_inspector.active()
+# tasks that have been assigned to a worker but have not started yet
+reserved_tasks = celery_inspector.reserved()
+# tasks that have been cancelled with 
+cancelled_tasks = celery_inspector.revoked()
+```
+
+Examples of outputs:
+```
+# active_tasks
+{'celery@27a8ec03a114': [{'id': '94be3755-011c-424e-a583-5fc73fd627ef', 'name': 'my_task_1', 'args': [], 'kwargs': {'n_clicks': 1}, 'type': 'my_task_1', 'hostname': 'celery@27a8ec03a114', 'time_start': 1737474277.8087084, 'acknowledged': True, 'delivery_info': {'exchange': '', 'routing_key': 'celery', 'priority': 0, 'redelivered': False}, 'worker_pid': 4385}]}
+# reserved/queued tasks
+{'celery@27a8ec03a114': [{'id': 'ab9eb6c7-643c-405d-a5ff-8aeb5ce6dac4', 'name': 'my_task_1', 'args': [], 'kwargs': {'n_clicks': 3}, 'type': 'my_task_1', 'hostname': 'celery@27a8ec03a114', 'time_start': None, 'acknowledged': False, 'delivery_info': {'exchange': '', 'routing_key': 'celery', 'priority': 0, 'redelivered': False}, 'worker_pid': None}, {'id': '44568f06-c78a-4c2a-9701-349a48ae9d47', 'name': 'my_task_2', 'args': [], 'kwargs': {'n_clicks': 1, 'len_min': 1}, 'type': 'my_task_2', 'hostname': 'celery@27a8ec03a114', 'time_start': None, 'acknowledged': False, 'delivery_info': {'exchange': '', 'routing_key': 'celery', 'priority': 0, 'redelivered': False}, 'worker_pid': None}]}
+# cancelled/revoked tasks
+{'celery@27a8ec03a114': ['210d6fe0-3c7c-4df2-8ec0-dc84d46869be', '6dfb3d24-2fca-4f27-98e4-1eb1832f4bc4']}
+```
+
+**Cancelling a task**
+```
+celery_app.control.revoke(task_id, terminate=True)
+```
+After we cancel a task, its id will appear in the list of revoked tasks, but its res.status might take a moment (1-2 seconds)
+to change from "PENDING" to "REVOKED". 
+"""
+
+app_description = html.Div(
+    dbc.Accordion(
+        [
+            dbc.AccordionItem(
+                dcc.Markdown(app_description_text),
+                title="Read more about this app",
+            )
+        ],
+        start_collapsed=True
+    )
+)
+
+task_description = dcc.Markdown("""
+You can:
+- Start two types of tasks and see how their status changes in the table. The celery process has two workers, so only two tasks (max) should show the 'Running' status at the same time.
+- Select one task and cancel it by clicking on the 'Cancel' button.
+- Get an overview of the tasks Celery is handling at any moment by clicking the 'Check Celery status and update table' button (the update can take up to 10 seconds and it will appear at the bottom of the window - scroll down if you don't see it). The table will get updated automatically every 5 seconds, and the 'Cancelled' status will appear immediately, but the 'Complete' and 'Running' status will only be updated by the interval or the button click.
+""")
+
+def celery_status_summary(celery_status_text):
+    update_time = datetime.datetime.now()
+    return html.Div(
+    dbc.Accordion(
+        [
+            dbc.AccordionItem(
+                dcc.Markdown(celery_status_text),
+                title="Celery status (last update: {update_time}",
+            )
+        ],
+        start_collapsed=True
+    )
+)
+
+def update_row_value(row_dict: dict, updated_values: dict):
+    updatedRow = row_dict.copy()
+    for k, v in updated_values.items():
+        updatedRow[k] = v
+    return updatedRow
+
+def get_celery_active_and_reserved(celery_inspector, celery_hostname):
+    celery_data = []
+    active_tasks = celery_inspector.active()
+    reserved_tasks = celery_inspector.reserved()
+    if active_tasks:
+        for t in active_tasks.get(celery_hostname, []):
+            if t != {}:
+                t["status"] = "Running"
+                if t["time_start"]:
+                    try :
+                        t["time_start"] = t["time_start"].strftime("%H:%M:%S")
+                    except : # if it's float the above code will fail
+                        t["time_start"] = datetime.datetime.fromtimestamp(t["time_start"]).strftime("%H:%M:%S")
+                celery_data.append(t)
+    if reserved_tasks:
+        for t in reserved_tasks.get(celery_hostname, []):
+            if t != {}:
+                t["status"] = "Queued"
+                if t["time_start"]:
+                    try :
+                        t["time_start"] = t["time_start"].strftime("%H:%M:%S")
+                    except : # if it's float the above code will fail
+                        t["time_start"] = datetime.datetime.fromtimestamp(t["time_start"]).strftime("%H:%M:%S")
+                celery_data.append(t)
+    
+    return celery_data
+
